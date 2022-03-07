aa829b08adf615b6fba8f47489f29dbcad61c52b
R3dner <matik_125@wp.pl>
2022-03-07 20:19:41 +0100

    beginning

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
-$sa = 1;
+
+    declare(strict_types=1);
+
+    namespace App;
+
+    require_once("src/utils/debug.php");
+
+    $action = $_GET['action'] ?? null;
+
 ?>
+
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <header>
+            <h1>Moje notatki</h1>
+        </header>
+
+        <container>
+            <menu>
+                <ul>
+                    <li>
+                        <a href="/">Lista notatek</a>
+                    </li>
+                    <li>
+                        <a href="/?action=create">Nowa notatka</a>
+                    </li>
+                </ul>
+            </menu>
+        </container>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
